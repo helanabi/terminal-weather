bf687b66a14a995d48320f7b78fce7c66b95906f
Hassan El anabi <abulkeram@proton.me>
2026-01-15 00:37:17 +0000

    Add command line arg for geocoordinates

--- terminal-weather/main.py
+++ terminal-weather/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
-import os
 import sys
+import util
 
 VERSION = "Terminal-weather 0.1.0"
 COPYRIGHT = """Copyright (C) 2026 Hassan El anabi
@@ -16,12 +16,13 @@ def parse_args():
         prog="weather",
         description="Get current weather and forecasts for upcoming days"
     )
-    parser.add_argument("when", nargs="?", default="today",
+
+    parser.add_argument("when", nargs="?",
                         choices=["now", "today", "tomorrow", "forecast"],
                         help="show weather data for the specified time period"
                         " (default: today)")
     parser.add_argument("-c", "--conf", help="configuration file")
-    parser.add_argument("-C", "--color", default="yes", choices=["yes", "no"],
+    parser.add_argument("-C", "--color", choices=["yes", "no"],
                         help="enable colored output (default: yes). "
                         "This option is ignored when -j/--json is used")
     parser.add_argument("-f", "--fields", help="specify a comma-separated list"
@@ -31,21 +32,59 @@ def parse_args():
     parser.add_argument("-j", "--json", action="store_true",
                         help="show results in raw json format")
     parser.add_argument("-k", "--key", help="OpenWeatherMap API key")
-    parser.add_argument("-l", "--location",
-                        help="must be in the format: city[,country]")
-    parser.add_argument("-u", "--units", default="metric",
+    parser.add_argument("-u", "--units",
                         choices=["metric", "imperial", "standard"],
                         help="(default: metric)")
     parser.add_argument("-v", "--version", action="store_true",
                         help="show software version and copyright notice")
+
+    ex_group = parser.add_mutually_exclusive_group()
+
+    ex_group.add_argument("-g", "--geocoordinates",
+                          help="geocoordintes of the form: latitude,longitude")
+    ex_group.add_argument("-l", "--location",
+                          help="a location of the format: city[,country]")
+
     args = parser.parse_args()
     return args
+
+def prompt(question):
+    answer = input(question).lower()
+    while answer not in ("yes", "no"):
+        answer = input("Please answer with 'yes' or 'no':").lower()
+    return answer
 
 def main(args):
     if args.version:
         print(VERSION)
         print(COPYRIGHT)
         sys.exit(0)
+
+    get_value = util.init_conf(args)
+
+    api_keys = get_value("key")
+    if not api_keys:
+        print("Error: unable to find an API key", file=sys.stderr)
+        sys.exit(3)
+
+    location = None
+    coords = None
+
+    if args.geocoordinates:
+        coords = args.geocoordinates
+    elif args.location:
+        location = args.location
+    elif get_value("geocoordinates"):
+        coords = get_value("geocoordinates")
+    elif get_value("location"):
+        location = args.location
+    else:
+        coords = util.guess_location(get_value)
+
+    if not (coords or location):
+        print("Error: one of 'geocoordinates' or 'location' must be specified"
+              " to get corresponding weather data", file=sys.stderr)
+        sys.exit(2)
 
 if __name__ == "__main__":
     main(parse_args())

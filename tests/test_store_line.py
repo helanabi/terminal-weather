f266606153706097766bddd6d73a34ffc2b0c1b4
Hassan El anabi <abulkeram@proton.me>
2026-02-07 07:53:13 +0000

    Fix test references

--- tests/test_store_line.py
+++ tests/test_store_line.py
@@ -1,34 +1,34 @@
 import re
-import util
 import pytest
+from terminal_weather import config
 
 class TestStoreLine:
 
     def test_singleton(self):
         conf = {}
-        util.store_line({ "singleton": ('a')}, conf, "a=b")
+        config.store_line({ "singleton": ('a')}, conf, "a=b")
         assert conf == { "a": "b" }
 
     def test_comulative(self):
         conf = {}
-        util.store_line({"cumulative": ('a')}, conf, "a=b")
+        config.store_line({"cumulative": ('a')}, conf, "a=b")
         assert conf == { "a": ["b"] }
 
     def test_space(self):
         conf = {}
-        util.store_line({"singleton": ('a')}, conf, "a = b")
+        config.store_line({"singleton": ('a')}, conf, "a = b")
         assert conf == { "a": "b" }
 
     def test_accumulation(self):
         conf = {}
         spec = { "cumulative": ('a') }
-        util.store_line(spec, conf, "a=b")
-        util.store_line(spec, conf, "a=c")
+        config.store_line(spec, conf, "a=b")
+        config.store_line(spec, conf, "a=c")
         assert conf == { "a": ["b", "c"] }        
 
     def test_unspecified_var(self, capsys):
         with pytest.raises(SystemExit):
-            util.store_line({}, {}, "a=b")
+            config.store_line({}, {}, "a=b")
 
         assert capsys.readouterr().err == \
             "Error: unrecognized configuration variable: a\n"
@@ -36,7 +36,7 @@ class TestStoreLine:
     def test_overwrite(self, capsys):
         conf = { 'a': 'b' }
         with pytest.raises(SystemExit):
-            util.store_line({ "singleton": ('a') }, conf, "a=c")
+            config.store_line({ "singleton": ('a') }, conf, "a=c")
 
         assert re.match("Error: multiple values .* variable: a\n",
                          capsys.readouterr().err)
@@ -51,7 +51,7 @@ class TestStoreLine:
         sample = ["a=0", "b=1", "c=2", "d=3", "c=4", "d=5"]
 
         for entry in sample:
-            util.store_line(spec, conf, entry)
+            config.store_line(spec, conf, entry)
 
         assert conf == {
             'a': '0',
